e365275d319bc913487337a875c8004230964da6
hj <dv.zinke@gmail.com>
2020-08-18 01:10:56 +0900

    feat : JPA MYSQL 에러로 인해 Column 추가 및 네이밍 형식 맞춤

--- src/main/java/com/dnd/jachwirus/write/domain/Document.java
+++ src/main/java/com/dnd/jachwirus/write/domain/Document.java
@@ -1,5 +1,7 @@
 package com.dnd.jachwirus.write.domain;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 import javax.persistence.*;
@@ -7,13 +9,30 @@ import javax.persistence.*;
 @Entity
 @Data
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Document {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
+    @Column(name = "`title`")
     public String title;
 
-    public String dataUrl;
+    @Column(name="`like`")
+    public Long like = 0L;
 
+    @Column(name = "`dis_like`")
+    public Long disLike = 0L;
+
+    @Column(name="`thumbnail_url`")
+    public String thumbnailUrl = null;
+
+    @OneToOne
+    @JoinColumn(name = "last_version")
+    public DocumentVersion lastVersion = null;
+
+    @Column(name = "`view_count`")
+    public Long viewCount = 0L;
+
+    public String category = null;
 }
